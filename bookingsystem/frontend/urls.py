e02233f579f6686b03ea3c9d16d387316b94d2f0
Markus Glavind <glavindmarkus@gmail.com>
2025-10-27 16:32:24 +0100

    Vol 2

--- bookingsystem/frontend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-import frontend.views as views
-
-# All urls will point to same HTML template because it is SPA application
-urlpatterns = [
-    path('', views.index, name='index'),
-]
